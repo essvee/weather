35cc55bfdcfce3f700b7203cb9924b0b483d6728
essvee <s.vincent@nhm.ac.uk>
2018-03-22 16:20:14 +0000

    api call working

--- src/Weather.kt
+++ src/Weather.kt
@@ -3,6 +3,9 @@ import java.io.FileReader
 import java.net.URL
 import java.time.LocalDate
 import java.util.*
+import java.time.format.DateTimeFormatter
+
+
 
 data class Specimen(val irn: Int, val collection: String, val collected_year: Int, val collected_month: Int,
                     val collected_day: Int, val dec_lat: Double,  val dec_long: Double, val lat_precision: Int,
@@ -50,11 +53,15 @@ class Weather (source: String) {
         return key
     }
 
-    fun getWeather(lat: Long, long: Long, start_date: String) {
-        val result = URL("http://cleanedobservations.wsi.com/CleanedObs.svc/premium?version=2" +
-                "&interval=daily&units=metric&format=csv&station=cfsr&userKey=$key" +
-                "&lat=39.6932&long=27.800399&startDate=01/01/2018&endDate=01/02/2018").readText()
-        print(result)
+    fun getWeather(lat: Double, long: Double, start_date: LocalDate) :String {
+        val endDay = start_date.plusDays(1)
+        val formatter = DateTimeFormatter.ofPattern("MM/dd/uuuu")
+
+        var result = URL("http://cleanedobservations.wsi.com/CleanedObs.svc/premium?version=2" +
+                "&interval=daily&units=metric&format=json&station=cfsr&userKey=$key" +
+                "&lat=$lat&long=$long&startDate=${start_date.format(formatter)}&endDate=${endDay.format(formatter)}")
+                .readText()
+        return result
     }
 
 }
