4bb888f7e1a545bccd765bea89231fd420722a2e
essvee <s.vincent@nhm.ac.uk>
2018-03-22 13:58:17 +0000

    adds tests

--- src/Weather.kt
+++ src/Weather.kt
@@ -1,59 +1,46 @@
 import java.io.BufferedReader
 import java.io.FileReader
-import java.io.IOException
 import java.util.*
 
-data class Specimen(val irn: String, val collection: String, val collected_day: Int, val collected_month: Int,
+data class Specimen(val irn: Int, val collection: String, val collected_day: Int, val collected_month: Int,
                     val collected_year: Int, val dec_lat: Double,  val dec_long: Double, val lat_precision: Int,
                     val long_precision: Int)
 
-fun main(args: Array<String>?) {
-    var fileReader: BufferedReader? = null
+class Weather (source: String) {
+    val specimens = ArrayList<Specimen>()
+    lateinit var key: String
 
-    try {
-        val specimens = ArrayList<Specimen>()
-        var line: String?
-
-        fileReader = BufferedReader(FileReader("src/portal_data_subset.csv"))
-
-        // Read CSV header
-        fileReader.readLine()
-
-        // Read the file line by line starting from the second line
-        line = fileReader.readLine()
-        while (line != null) {
-            val tokens = line.split(",")
-            if (tokens.isNotEmpty()) {
-                val specimen = Specimen(
-                        tokens[0],
-                        tokens[1],
-                        tokens[2].toInt(),
-                        tokens[3].toInt(),
-                        tokens[4].toInt(),
-                        tokens[5].toDouble(),
-                        tokens[6].toDouble(),
-                        tokens[7].toInt(),
-                        tokens[8].toInt()
-                        )
-                specimens.add(specimen)
-            }
-
-            line = fileReader.readLine()
-            }
-
-        // Print the new customer list
-        for (s in specimens) {
-            println(s)
-        }
-    } catch (e: Exception) {
-        println("Reading CSV Error!")
-        e.printStackTrace()
-    } finally {
+    init {
         try {
-            fileReader!!.close()
-        } catch (e: IOException) {
-            println("Closing fileReader Error!")
+            val fileReader = BufferedReader(FileReader(source))
+            fileReader.use {
+
+                // Read header
+                fileReader.readLine()
+
+                // Read the file line by line starting from the second line
+                var line = fileReader.readLine()
+                while (line != null) {
+                    val tokens = line.split(",")
+                    // Use values to create specimen and add to list
+                    if (!tokens.isEmpty()) {
+                        specimens.add(Specimen(tokens[0].toInt(), tokens[1], tokens[2].toInt(), tokens[3].toInt(),
+                                tokens[4].toInt(), tokens[5].toDouble(), tokens[6].toDouble(), tokens[7].toInt(),
+                                tokens[8].toInt()))
+                    }
+                    line = fileReader.readLine()
+                }
+            }
+        val keyReader = BufferedReader(FileReader("src/apikey"))
+        keyReader.use {
+            key = keyReader.readLine()
+        }
+        } catch (e: Exception) {
+            println("Reading CSV Error!")
             e.printStackTrace()
         }
     }
+
+
 }
+
---
+++ test/WeatherTest.kt
@@ -0,0 +1,28 @@
+/**
+ * Created by svince04 on 22/03/2018 for weather.
+ */
+import org.junit.*
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+class WeatherTest {
+    lateinit var weather: Weather
+
+    @Before
+    fun initial() {
+        weather = Weather("src/portal_data_subset.csv")
+    }
+
+    @Test
+    fun readFile() {
+        val expected = 1581
+        assertEquals(expected, weather.specimens.size)
+    }
+
+    @Test
+    fun testSpecimenIRN() {
+        val expected = 4770704
+        assertEquals(expected, weather.specimens[0].irn)
+    }
+}
