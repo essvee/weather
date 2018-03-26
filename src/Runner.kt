390780ca1af1b606e3f9167ee6ba471f45e33dd3
essvee <s.vincent@nhm.ac.uk>
2018-03-26 18:06:49 +0100

    starting to parse api response...

---
+++ src/Runner.kt
@@ -0,0 +1,5 @@
+import com.google.gson.Gson
+
+fun main(args: Array<String>) {
+    val weather = Weather("src/portal_data_subset.csv")
+    }
--- src/Weather.kt
+++ src/Weather.kt
@@ -2,38 +2,77 @@ import java.io.BufferedReader
 import java.io.FileReader
 import java.net.URL
 import java.time.LocalDate
-import java.util.*
 import java.time.format.DateTimeFormatter
 
-
-
-data class Specimen(val irn: Int, val collection: String, val collected_year: Int, val collected_month: Int,
-                    val collected_day: Int, val dec_lat: Double,  val dec_long: Double, val lat_precision: Int,
-                    val long_precision: Int, val date: LocalDate)
-
 class Weather (source: String) {
-    val specimens = ArrayList<Specimen>()
+    val specimens: ArrayList<ArrayList<String>> = arrayListOf<ArrayList<String>>()
+    private val csvHeader = "irn,collection_code,collected_year,collected_month,collected_day," +
+            "decimal_latitude,decimal_longitude,lat_precision,long_precision,date,SiteId,Latitude,Longitude," +
+            "Day,Month,Year,MinSurfaceTemperatureCelsius,MaxSurfaceTemperatureCelsius,AvgSurfaceTemperatureCelsius," +
+            "MinSurfaceDewpointTemperatureCelsius,MaxSurfaceDewpointTemperatureCelsius," +
+            "AvgSurfaceDewpointTemperatureCelsius,MinSurfaceWetBulbTemperatureCelsius," +
+            "MaxSurfaceWetBulbTemperatureCelsius,AvgSurfaceWetBulbTemperatureCelsius," +
+            "MinRelativeHumidityPercent,MaxRelativeHumidityPercent,AvgRelativeHumidityPercent," +
+            "MinSurfaceAirPressureKilopascals,MaxSurfaceAirPressureKilopascals,AvgSurfaceAirPressureKilopascals," +
+            "MinCloudCoveragePercent,MaxCloudCoveragePercent,AvgCloudCoveragePercent,MinWindChillTemperatureCelsius," +
+            "MaxWindChillTemperatureCelsius,AvgWindChillTemperatureCelsius,MinApparentTemperatureCelsius," +
+            "MaxApparentTemperatureCelsius,AvgApparentTemperatureCelsius,MinWindSpeedKph,MaxWindSpeedKph," +
+            "AvgWindSpeedKph,MinWindDirectionDegrees,MaxWindDirectionDegrees,AvgWindDirectionDegrees," +
+            "MinPrecipitationPreviousHourCentimeters,MaxPrecipitationPreviousHourCentimeters," +
+            "AvgPrecipitationPreviousHourCentimeters,SumPrecipitationPreviousHourCentimeters," +
+            "MinDownwardSolarRadiationWsqm,MaxDownwardSolarRadiationWsqm,AvgDownwardSolarRadiationWsqm," +
+            "SumDownwardSolarRadiationWsqm,MinDiffuseHorizontalRadiationWsqm,MaxDiffuseHorizontalRadiationWsqm," +
+            "AvgDiffuseHorizontalRadiationWsqm,SumDiffuseHorizontalRadiationWsqm,MinDirectNormalIrradianceWsqm," +
+            "MaxDirectNormalIrradianceWsqm,AvgDirectNormalIrradianceWsqm,SumDirectNormalIrradianceWsqm," +
+            "MinMslPressureKilopascals,MaxMslPressureKilopascals,AvgMslPressureKilopascals,MinHeatIndexCelsius," +
+            "MaxHeatIndexCelsius,AvgHeatIndexCelsius,MinSnowfallCentimeters,MaxSnowfallCentimeters," +
+            "AvgSnowfallCentimeters,SumSnowfallCentimeters,MinSurfaceWindGustsKph,MaxSurfaceWindGustsKph," +
+            "AvgSurfaceWindGustsKph,MinPotentialEvapotranspirationMicrometersPerHour," +
+            "MaxPotentialEvapotranspirationMicrometersPerHour,AvgPotentialEvapotranspirationMicrometersPerHour," +
+            "MinSurfaceWaterRunOffMillimeters,MaxSurfaceWaterRunOffMillimeters,AvgSurfaceWaterRunOffMillimeters," +
+            "SumSurfaceWaterRunOffMillimeters,MinTenToFortyLiquidSoilMoisturePercent," +
+            "MaxTenToFortyLiquidSoilMoisturePercent,AvgTenToFortyLiquidSoilMoisturePercent," +
+            "MinTenToFortySoilTemperatureCelsius,MaxTenToFortySoilTemperatureCelsius," +
+            "AvgTenToFortySoilTemperatureCelsius,MinZeroToTenLiquidSoilMoisturePercent," +
+            "MaxZeroToTenLiquidSoilMoisturePercent,AvgZeroToTenLiquidSoilMoisturePercent," +
+            "MinZeroToTenSoilTemperatureCelsius,MaxZeroToTenSoilTemperatureCelsius,AvgZeroToTenSoilTemperatureCelsius," +
+            "MinFortyToOneHundredLiquidSoilMoisturePercent,MaxFortyToOneHundredLiquidSoilMoisturePercent," +
+            "AvgFortyToOneHundredLiquidSoilMoisturePercent,MinFortyToOneHundredSoilTemperatureCelsius," +
+            "MaxFortyToOneHundredSoilTemperatureCelsius,AvgFortyToOneHundredSoilTemperatureCelsius"
     lateinit var key: String
 
     init {
+        setKey()
         try {
+            // Get specimen records
             val fileReader = BufferedReader(FileReader(source))
-            fileReader.use {
 
+            fileReader.use {
                 // Read header
                 fileReader.readLine()
-
                 // Read the file line by line starting from the second line
-                var line = fileReader.readLine()
-                while (line != null) {
-                    val tokens = line.split(",")
-                    // Use values to create specimen and add to list
+                fileReader.forEachLine {
+
+                    val tokens = it.split(",")
+                    val startDate: LocalDate = LocalDate.parse(tokens[9], DateTimeFormatter.ofPattern("dd/MM/uuuu"))
+                    val endDate:LocalDate = startDate.plusDays(1)
+
+                    // Use read values to create specimen record and add to list
                     if (!tokens.isEmpty()) {
-                        specimens.add(Specimen(tokens[0].toInt(), tokens[1], tokens[2].toInt(), tokens[3].toInt(),
-                                tokens[4].toInt(), tokens[5].toDouble(), tokens[6].toDouble(), tokens[7].toInt(),
-                                tokens[8].toInt(), LocalDate.parse(tokens[9])))
+                        val specimenRecord = arrayListOf(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4],
+                                tokens[5], tokens[6], tokens[7],tokens[8], tokens[9])
+
+                        // Date formatter for api call
+                        val ibmFormatter = DateTimeFormatter.ofPattern("MM/dd/uuuu")
+                        val resultList = URL("http://cleanedobservations.wsi.com/CleanedObs.svc/premium?version=2" +
+                                "&interval=daily&units=metric&format=csv&station=cfsr&userKey=$key" +
+                                "&lat=${tokens[5]}&long=${tokens[5]}&startDate=${startDate.format(ibmFormatter)}" +
+                                "&endDate=${endDate.format(ibmFormatter)}").readText().substringAfter("\n").split(",")
+
+                        // Add results onto end of specimen arraylist + add into main list
+                        specimenRecord.addAll(resultList)
+                        specimens.add(specimenRecord)
                     }
-                    line = fileReader.readLine()
                 }
             }
         } catch (e: Exception) {
@@ -42,26 +81,16 @@ class Weather (source: String) {
         }
     }
 
-    fun readKey(): String {
+
+    fun setKey() {
         try {
+            // Get key for API
             val keyReader = BufferedReader(FileReader("src/apikey"))
             keyReader.use { key = keyReader.readLine() }
         } catch (e: Exception) {
-            println("Reading key Error!")
+            println("Couldn't retrieve key!")
             e.printStackTrace()
         }
-        return key
-    }
-
-    fun getWeather(lat: Double, long: Double, start_date: LocalDate) :String {
-        val endDay = start_date.plusDays(1)
-        val formatter = DateTimeFormatter.ofPattern("MM/dd/uuuu")
-
-        var result = URL("http://cleanedobservations.wsi.com/CleanedObs.svc/premium?version=2" +
-                "&interval=daily&units=metric&format=json&station=cfsr&userKey=$key" +
-                "&lat=$lat&long=$long&startDate=${start_date.format(formatter)}&endDate=${endDay.format(formatter)}")
-                .readText()
-        return result
     }
 
 }
